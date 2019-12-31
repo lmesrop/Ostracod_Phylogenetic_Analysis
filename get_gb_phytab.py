f291637459362f31034d9ba8e9ec80b8a4ea87bf
Lisa Mesrop <lmesrop@pod-login1.podcluster>
2019-12-31 11:35:53 -0800

    Script to pull fasta files from Genbank using accession numbers and reformatting fasta files to phytab format.

---
+++ get_gb_phytab.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+#!/usr/bin/python
+#This script does two things:(1) Grab accession files from Genbank (2) Reformat fasta file to phytab file. 
+#USAGE: python ./get_gb_phytab.py
+#Author: Lisa Mesrop
+
+
+# In[4]:
+
+
+from Bio import Entrez
+from Bio import SeqIO
+
+import os
+import re
+import time 
+
+dir_path = os.path.join("data", "accessions") 
+out_dir_path = os.path.join("data", "sequences")
+
+onlyfiles = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
+
+for filename in onlyfiles: 
+    filepath = os.path.join(dir_path, filename)
+    
+    
+    with open(filepath) as fp: 
+        outfile = filename.replace('.txt', '.phytab') 
+        outpath = os.path.join(out_dir_path, outfile)
+
+        lines = fp.readlines() 
+        gname = lines[0].strip() 
+        
+        accessions = [] 
+        for line in lines[1:]: 
+            accessions.append(line.strip())
+            
+        Entrez.email = "lmesrop@ucsb.edu"    
+        handle = Entrez.efetch(db="nucleotide", id=accessions, rettype="fasta", retmode="text")
+        record = SeqIO.parse(handle, "fasta")
+        records = list(record)
+        
+        with open(outpath, 'w') as fp:
+
+            for row in records:
+                rec_id = re.findall("([^\.]*)", row.id)[0]
+                if "cf." in row.description:
+                    species = re.findall("\S* (\S* \S* \S*)", row.description)[0] 
+                else:
+                    species = re.findall("\S* (\S* \S*)", row.description)[0]
+                species = species.replace(" ", "_")
+                #gname = re.findall(" ([A-Z0-9]{3})", row.description)[0]
+                seq = row.seq
+
+                line = "{}\t{}\t{}\t{}\n".format(species, gname, rec_id, seq)
+                fp.write(line)
+
+
+# In[ ]:
+
+
+
+
